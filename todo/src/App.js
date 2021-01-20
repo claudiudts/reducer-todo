18032bf596cd2b10818421195a18bfe280caff1b
Claudiu Sarb <sarb.claudiu391@gmail.com>
2021-01-19 19:20:37 -0600

    static data working

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todos = [{
+    title: 'todo 1',
+    id: 0,
+    completed: false
+  },
+  {
+    title: 'todo 2',
+    id: 1,
+    completed: false
+  },
+  {
+    title: 'todo 3',
+    id: 2,
+    completed: false
+  }]
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Todo List!</h1>
+     <div className="todoList">
+       <h3>List of Tasks for today!</h3>
+       <ul>
+         {
+           todos.map(todo => {
+             return(<li>{todo.title}</li>)
+           })
+         }
+       </ul>
+       <button>Clear Completed!</button>
+     </div>
+      <div className='todoForm'>
+        <form>
+          <labe>
+            What is your task?
+            <input />
+            <button>Make it happen</button>
+          </labe>
+        </form>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+//The app will have a list of things to do
+// also an add button with an input field  
+// and a clear button
+//
